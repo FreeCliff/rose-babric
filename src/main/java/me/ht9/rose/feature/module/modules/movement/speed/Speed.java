4b9ee42aa87c243b5cec81d3dff66439ca5d2925
skibidiware <skibidiware@nathatpas.tel>
2024-11-10 18:20:04 +0100

    Add unfinished logger/rat of some kind

--- src/main/java/me/ht9/rose/feature/module/modules/movement/speed/Speed.java
+++ src/main/java/me/ht9/rose/feature/module/modules/movement/speed/Speed.java
@@ -1,7 +1,6 @@
 package me.ht9.rose.feature.module.modules.movement.speed;
 
 import me.ht9.rose.event.bus.annotation.SubscribeEvent;
-import me.ht9.rose.event.events.PosRotUpdateEvent;
 import me.ht9.rose.event.events.TickEvent;
 import me.ht9.rose.feature.module.Module;
 import me.ht9.rose.feature.module.annotation.Description;
@@ -11,13 +10,13 @@ import me.ht9.rose.util.module.Timer;
 import net.minecraft.src.Packet19EntityAction;
 
 @SuppressWarnings("unused")
-@Description(".")
+@Description("Go fast")
 public class Speed extends Module
 {
     private static final Speed instance = new Speed();
 
     public final Setting<Type> type = new Setting<>("Type", Type.NoCheat);
-    public final Setting<Double> speed = new Setting<>("Speed", 0.0, 0.5, 1.0, 1);
+    public final Setting<Double> speed = new Setting<>("Speed", 1.0, 5.0, 20.0, 1);
 
     private final Timer timer = new Timer();
 
@@ -29,17 +28,20 @@ public class Speed extends Module
     @SubscribeEvent
     public void onTick(TickEvent event)
     {
-        Movement.setSpeed(
-                mc.thePlayer.movementInput.moveForward != 0 || mc.thePlayer.movementInput.moveStrafe != 0
-                        ? speed.value()
-                        : 0
-        );
-
-        if (this.type.value() != Type.NoCheat) return;
-        if (this.timer.hasReached(450)) return;
-
-        mc.getSendQueue().addToSendQueue(new Packet19EntityAction(mc.thePlayer, 3));
-        this.timer.reset();
+        if (this.type.value() == Type.Vanilla)
+        {
+            Movement.setSpeed(
+                    mc.thePlayer.movementInput.moveForward != 0 || mc.thePlayer.movementInput.moveStrafe != 0
+                            ? speed.value()
+                            : 0
+            );
+        }
+        else if (this.type.value() == Type.NoCheat)
+        {
+            if (!this.timer.hasReached(450)) return;
+            mc.getSendQueue().addToSendQueue(new Packet19EntityAction(mc.thePlayer, 3));
+            this.timer.reset();
+        }
     }
 
     public static Speed instance()
