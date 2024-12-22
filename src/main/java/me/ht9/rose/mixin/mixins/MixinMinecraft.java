c1a3b88b52f7fa9d53036390e382d6bc574af2a6
skibidiware <178638608+skibidiware@users.noreply.github.com>
2024-12-22 10:15:19 +0100

    Injectmaxxing

--- src/main/java/me/ht9/rose/mixin/mixins/MixinMinecraft.java
+++ src/main/java/me/ht9/rose/mixin/mixins/MixinMinecraft.java
@@ -7,10 +7,13 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.MovingObjectPosition;
 import net.minecraft.src.World;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static org.lwjgl.opengl.GL11.*;
